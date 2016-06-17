d7fd6cb473d25b16b3c2700218ac6685a2cdae07
Victor Kurauchi <victorkurauchi@gmail.com>
2016-06-17 11:45:03 -0300

    Is there an achievement for this?

--- README.md
+++ README.md
@@ -8,3 +8,17 @@ Running this script you'll be able to output the desired json.
 
 ## Why ?
 Because if you end up using node-i18n, you'll probably want to use this lib.
+
+## Running
+
+`node index.js --destiny=espanol --input=espanol.xlsx --output=es.json`
+
+## Properties
+
+*destiny* it's the translated columns header (example: we want to translate to spanish, and the .xlsx needs to contain this header name)
+
+*origin* it's our origin language (.xlsx file needs to contain this header name)
+
+*input* .xlsx file location 
+
+*output* our translated file in json
--- index.js
+++ index.js
@@ -3,26 +3,47 @@
 const XLSX = require('xlsx');
 const _ = require('underscore');
 const fs = require('fs');
+const yargs = require('yargs').argv;
 
-let workbook = XLSX.readFile('lang.xlsx');
-var first_sheet_name = workbook.SheetNames[0];
+
+// let headerOrigin = yargs.origin || 'pt-br';
+// let headerDestiny = yargs.destiny || 'english';
+// let input = yargs.input || 'lang.xlsx';
+// let output = yargs.output || 'lang.json';
+
+let 
+[
+  headerOrigin, 
+  headerDestiny, 
+  input, 
+  output
+] = 
+[
+  yargs.origin || 'pt-br', 
+  yargs.destiny || 'english', 
+  yargs.input || 'lang.xlsx', 
+  yargs.output || 'lang.json'
+]
+
+let workbook = XLSX.readFile(input);
+let first_sheet_name = workbook.SheetNames[0];
  
 /* Get worksheet */
-var worksheet = workbook.Sheets[first_sheet_name];
-var headers = {};
-var data = [];
+let worksheet = workbook.Sheets[first_sheet_name];
+let headers = {};
+let data = [];
 
 for (var z in worksheet) {
   if(z[0] === '!') continue;
   //parse out the column, row, and value
-  var col = z.substring(0,1);
-  var row = parseInt(z.substring(1));
-  var value = worksheet[z].v;
+  let col = z.substring(0,1);
+  let row = parseInt(z.substring(1));
+  let value = worksheet[z].v;
 
   //store header names
   if(row == 1) {
-      headers[col] = value;
-      continue;
+    headers[col] = value;
+    continue;
   }
 
   if(!data[row]) data[row]={};
@@ -33,13 +54,16 @@ for (var z in worksheet) {
 data.shift();
 data.shift();
 
-var transformed = {};
+console.log(headerOrigin, headerDestiny);
+let transformed = {};
 
 _.each(data, (value, key, list) => {
   _.each(value, (v, k, l) => {
-    transformed[list[key]['pt-br']] = list[key]['english']
+    // console.log(list[key]['espanol'])
+    // transformed[list[key]['pt-br']] = list[key]['english']
+    transformed[list[key][headerOrigin]] = list[key][headerDestiny]
   }); 
 })
 
 
-fs.writeFileSync('en.json', JSON.stringify(transformed));
+fs.writeFileSync(output, JSON.stringify(transformed));
